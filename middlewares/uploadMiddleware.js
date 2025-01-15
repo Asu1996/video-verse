45ce03d6e778752ad4b1b581872ccd1ab76513f0
Asu1996 <asutoshrt96@gmail.com>
2025-01-15 23:55:36 +0530

    refactor: moved upload middleware to separate file

---
+++ middlewares/uploadMiddleware.js
@@ -0,0 +1,27 @@
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+
+const { maxFileSize } = require('../config')
+
+const uploadDir = path.join(__dirname, '../uploads')
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true })
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir)
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, uniqueSuffix + path.extname(file.originalname))
+  },
+})
+
+const uploadMiddleware = multer({
+  storage,
+  limits: { fileSize: maxFileSize },
+}).single('videoFile')
+
+module.exports = { uploadMiddleware }
--- routes/video/index.js
+++ routes/video/index.js
@@ -1,10 +1,6 @@
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
 const router = require('express').Router()
 
 const { uploadController } = require('../../controllers/uploadController')
-const { maxFileSize } = require('../../config')
 const { getAllController } = require('../../controllers/getAllController')
 const { trimController } = require('../../controllers/trimController')
 // const { mergeController } = require('../../controllers/mergeController')
@@ -13,27 +9,8 @@ const {
   generateLinkController,
   accessVideoController,
 } = require('../../controllers/linkSharingController')
-const { authMiddleware } = require('../../middlewares/auth')
-
-const uploadDir = path.join(__dirname, '../../uploads')
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true })
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir)
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-    cb(null, uniqueSuffix + path.extname(file.originalname))
-  },
-})
-
-const uploadMiddleware = multer({
-  storage,
-  limits: { fileSize: maxFileSize },
-}).single('videoFile')
+const { authMiddleware } = require('../../middlewares/authMiddleware')
+const { uploadMiddleware } = require('../../middlewares/uploadMiddleware')
 
 router.use(authMiddleware);
 
